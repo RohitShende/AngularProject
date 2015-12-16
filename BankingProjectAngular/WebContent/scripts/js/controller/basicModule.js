d6bd709b915d5ec38a89597fef92269233fc88fe
navinmaheshwari305 <navin.maheshwari305@gmail.com>
2015-12-16 22:25:08 +0530

    Logout Functionality rohit

--- BankingProjectAngular/WebContent/scripts/js/controller/basicModule.js
+++ BankingProjectAngular/WebContent/scripts/js/controller/basicModule.js
@@ -5,14 +5,35 @@
 	var app = angular.module('basicModule', []);
 
 	app.controller("logoutController", function($scope, $rootScope,
-			$localStorage, $location) {
+			$localStorage, $location,toaster , $http) {
 
-		/*if ($localStorage.role == 'admin') {
-		} else  if($localStoarge.role=='branchManager'){
-		}*/
-
-		delete $localStorage.role;
+		/*
+		 * if ($localStorage.role == 'admin') { } else
+		 * if($localStoarge.role=='branchManager'){ }
+		 */
 		$scope.$storage = $localStorage;
-		$location.path("\home");
+		$http({
+			method : 'put',
+			url : $scope.$storage.baseURI+$scope.$storage.role+"/logout",
+			headers : {
+				'Content-Type' : 'application/json'
+			},
+			data : {
+				role : $scope.$storage.role,
+				id : $scope.$storage.id
+			}
+		}).then(
+				function successCallback(response) {
+					toaster.pop('success',"Logout","Successfully Logout");
+					delete $localStorage.role;
+					$location.path("\home");
+					
+				}, function errorCallback(response) {
+					toaster.pop('success',"Logout","Successfully not Logout");
+//					/delete $localStorage.role;
+					$location.path("\home");
+				});
+
+
 	});
 })();
