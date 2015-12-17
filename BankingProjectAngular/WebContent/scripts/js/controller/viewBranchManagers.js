fe65f032780a7b4c218ce3926af5db21114e7e18
sharifulislam21 <sharifulislammallik@gmail.com>
2015-12-17 10:21:22 +0530

    Updated viewBranch Updated viewBranchManagers

--- BankingProjectAngular/WebContent/scripts/js/controller/viewBranchManagers.js
+++ BankingProjectAngular/WebContent/scripts/js/controller/viewBranchManagers.js
@@ -1,14 +1,43 @@
 /***	Common Elements - Header / Footer	***/
 /** * Directives ** */
 
+angular.module('inbApp').controller('PaginationDemoCtrl', function ($scope, $log) {
+});
+
 var app = angular.module('viewBranchManagers', []);
 app.controller("viewBranchManagersController", function($scope, $log,
 		$stateParams, $location, $state, $rootScope, $http) {
+	
+	 $scope.currentPage = 0;
+	 $scope.pageSize = 5;
+	 $scope.numberOfPages=50;
+	 $http.get('http://localhost:8080/branchmanager/').then(function(data){
+		$scope.numberOfPages=$scope.branchManagers.length/$scope.pageSize;
+	 });
+	$scope.setPage = function (pageNo) {
+		  console.log("set page called")
+		  $scope.currentPage = pageNo;
+	  };
+	  
+	  $scope.pageChanged = function() {
+		  console.log("page changed"+$scope.currentPage);
+		  $http.get(url+($scope.currentPage-1)*parseInt($scope.pageSize)+'/'+$scope.pageSize).success(function(data, status) {
+			    //$scope.data = data;
+			    $scope.branchManagers = data;
+			    $rootScope.size=$scope.branchManagers.length;
+			    console.log("rootscope "+$rootScope.size);
+			    $log.log($scope.data); 
+		});  
+	    $log.log('Page changed to: ' + $scope.currentPage);
+	  };
+	  
+	
 	console.log($scope.$storage);
 	var url = $scope.$storage.baseURI + 'branchmanager/'
 	console.log(url)
 	$scope.branchManagers = [];
-	$http.get(url).success(function(data, status) {
+	
+	$http.get(url+$scope.currentPage*parseInt($scope.pageSize)+'/'+$scope.pageSize).success(function(data, status) {
 	    //$scope.data = data;
 	    $scope.branchManagers = data;
 	    $rootScope.size=$scope.branchManagers.length;
--- BankingProjectAngular/WebContent/scripts/js/controller/viewBranches.js
+++ BankingProjectAngular/WebContent/scripts/js/controller/viewBranches.js
@@ -1,21 +1,31 @@
 /***	Common Elements - Header / Footer	***/
 /** * Directives ** */
 
+/***	Common Elements - Header / Footer	***/
+/** * Directives ** */
+
 angular.module('inbApp').controller('PaginationDemoCtrl', function ($scope, $log) {
 });
 
 var app = angular.module('viewBranches', []);
 app.controller("viewBranchesController", function($scope, $log,
 		$stateParams, $location, $state, $rootScope, $http) {
-
-	  $scope.setPage = function (pageNo) {
-		  console.log("set page called")
-		  $scope.bigCurrentPage = pageNo;
+		
+	 $scope.currentPage = 0;
+	 $scope.pageSize = 5;
+	 $http.get('http://localhost:8080/branch/').then(function(data){
+		 console.log(data)
+		$scope.numberOfPages=$scope.branches.length;
+	 });
+		 
+	 $scope.setPage = function (pageNo) {
+		  console.log("set page called"+pageNo);
+			  $scope.currentPage = pageNo;
 	  };
 
 	  $scope.pageChanged = function() {
-		  console.log("page changed"+$scope.bigCurrentPage);
-		  $http.get(url+parseInt($scope.bigCurrentPage)*5+"/5").success(function(data, status) {
+		  console.log("page changed"+$scope.currentPage);
+		  $http.get(url+($scope.currentPage-1)*parseInt($scope.pageSize)+'/'+$scope.pageSize).success(function(data, status) {
 			    //$scope.data = data;
 			    $scope.branches = data;
 			    $rootScope.size=$scope.branches.length;
@@ -25,16 +35,13 @@ app.controller("viewBranchesController", function($scope, $log,
 	    $log.log('Page changed to: ' + $scope.currentPage);
 	  };
 
-	  $scope.maxSize = 5;
-	  $scope.bigTotalItems = 175;
-	  $scope.bigCurrentPage = 1;
 
 	console.log($scope.$storage);
 	var url = $scope.$storage.baseURI + 'branch/'
 	console.log(url)
 	$scope.branches = [];
 	
-	$http.get(url+"0/5").success(function(data, status) {
+	$http.get(url+$scope.currentPage*parseInt($scope.pageSize)+'/'+$scope.pageSize).success(function(data, status) {
 	    //$scope.data = data;
 	    $scope.branches = data;
 	    $rootScope.size=$scope.branches.length;
@@ -43,6 +50,7 @@ app.controller("viewBranchesController", function($scope, $log,
 	});
 
 });
+
 
 
 
